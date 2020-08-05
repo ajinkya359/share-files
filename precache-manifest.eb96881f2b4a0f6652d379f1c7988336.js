d2ead8216924bc6c3d13b8689b960cfea6a0b65f
ajinkya359 <56084456+ajinkya359@users.noreply.github.com>
2020-08-05 16:18:36 +0530

    Updates

---
+++ precache-manifest.eb96881f2b4a0f6652d379f1c7988336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685b1111c065eccf4876e72f6e78136e",
+    "url": "/share-files/index.html"
+  },
+  {
+    "revision": "71928e21257036bfaba3",
+    "url": "/share-files/static/css/main.4e254137.chunk.css"
+  },
+  {
+    "revision": "3e4beaba2ed5f3722c29",
+    "url": "/share-files/static/js/2.0d66a97f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/share-files/static/js/2.0d66a97f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71928e21257036bfaba3",
+    "url": "/share-files/static/js/main.779025e2.chunk.js"
+  },
+  {
+    "revision": "41e2a759d15723ae580b",
+    "url": "/share-files/static/js/runtime-main.de7ad16e.js"
+  }
+]);
